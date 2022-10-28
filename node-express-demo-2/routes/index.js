4cb748d0355f6a2dfe3f81ccc0d72544001877d0
Lance <phiferthree@gmail.com>
2022-10-28 11:30:04 -0700

    t

--- node-express-demo-2/routes/index.js
+++ node-express-demo-2/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  console.log("test-2");
+  console.log("2");
   res.render("index", { title: "Express", name: process.env.NAME });
 });
 
